8ab33b66179acc4c8c9b085e950bfdca22dc84f0
Mik133 <michaelpopov133@gmail.com>
2025-01-25 15:40:15 +0200

    Added folder creation in main

--- Junction_sim/carFlowObject.py
+++ Junction_sim/carFlowObject.py
@@ -4,6 +4,7 @@ class carFlowObject:
     def __init__(self,f_id,f_begin,f_from,f_to,f_end,f_vhs_per_hour):
         self.f_id = f_id
         self.f_begin = f_begin
+        self.f_depart_from = 'random'
         self.f_from = f_from
         self.f_to = f_to
         self.f_end = f_end
@@ -13,6 +14,7 @@ class carFlowObject:
         car_flow_xml = net_xml.createElement('flow')
         car_flow_xml.setAttribute('id',self.f_id)
         car_flow_xml.setAttribute('begin',self.f_begin)
+        car_flow_xml.setAttribute('departLane',self.f_depart_from)
         car_flow_xml.setAttribute('from',self.f_from)
         car_flow_xml.setAttribute('to',self.f_to)
         car_flow_xml.setAttribute('end',self.f_end)
--- sumo_main_runner.py
+++ sumo_main_runner.py
@@ -1,14 +1,33 @@
 #---Written by Michael Popov and Yuval Marsh---
 
+# Imports
 from Simple_road_sim.simple_sim_main import simple_sim_maker
 from Junction_sim.junction_sim_main import split_sim_maker
+import os
 # Constants:
 SIMPLE_SIM = 'simple'
 SPLIT_ROAD_SIM = 'split'
+# Folder names:
+SUMO_SIM_FILES_FOLDER = "Sumo sim files"
+SIMPLE_SIM_FOLDER = 'Simple road'
+INTERSECTION_SIM_FOLDER = 'Intersection road'
+GO_ONE_FOLDER_BACK = '..'
 
 if __name__ == '__main__':
     simp_type = SPLIT_ROAD_SIM
+    if not os.path.isdir(SUMO_SIM_FILES_FOLDER):
+        os.mkdir(SUMO_SIM_FILES_FOLDER)
+    os.chdir(SUMO_SIM_FILES_FOLDER)
     if simp_type == SIMPLE_SIM:
+        if not os.path.isdir(SIMPLE_SIM_FOLDER):
+            os.mkdir(SIMPLE_SIM_FOLDER)
+        os.chdir(SIMPLE_SIM_FOLDER)
         simple_sim_maker(150,0,'E0','E1','J0','J1','13.89',4,10,25,0.25)
+        os.chdir(GO_ONE_FOLDER_BACK)
     elif simp_type == SPLIT_ROAD_SIM:
+        if not os.path.isdir(INTERSECTION_SIM_FOLDER):
+            os.mkdir(INTERSECTION_SIM_FOLDER)
+        os.chdir(INTERSECTION_SIM_FOLDER)
         split_sim_maker()
+        os.chdir(GO_ONE_FOLDER_BACK)
+    os.chdir(GO_ONE_FOLDER_BACK)
